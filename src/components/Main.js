59738b0be598c6ad148f768603bef8fe60cc7dc6
Laith alnatour <laithalnatour92@gmail.com>
2022-07-26 23:33:21 +0300

    Solve Lab: Class 03

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,15 @@
 import React from "react";
+import Cards from "./HornedBeast";
 import beast1  from "../images/Rhino.jpg";
 import beast2  from "../images/Bison .jpg";
-import Cards from "./Cards";
-
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
 let horned1 = require('./data.json');
 
 let mythicalBeasts = [
     {
+        "_id": 21,
         "image_url": beast1,
         "title": "Rhino",
         "description": "is a member of any of the five extant species (or numerous extinct species) of odd-toed ungulates in the family Rhinocerotidae",
@@ -16,7 +18,6 @@ let mythicalBeasts = [
     },
 
     {
-        
         "image_url": beast2,
         "title": "Bison",
         "description": "Bison are large bovines in the genus Bison within the tribe Bovini. Two extant species and numerous extinct species are recognised.",
@@ -24,17 +25,46 @@ let mythicalBeasts = [
         "horns": 2
     }];
 
-    let horned = horned1.concat(mythicalBeasts);
+    let horned2 = horned1.concat(mythicalBeasts);
 
 
 
 class Main extends React.Component {
-
-    render() {
-        return (
+    constructor(props) {
+        super(props);
+        this.state = {value: ''
+        };
+    
         
-            horned.map( beast => 
-                <Cards title={beast.title} image_url={beast.image_url} description={beast.description} />)
+      }
+    
+
+      
+      findHorned = (e) => {
+          console.log(e.target.value)
+          return this.setState({value: e.target.value}); 
+
+      }
+      render() {
+          console.log(this.state.value);
+          
+          return (
+              <div> 
+                <input type="text" placeholder="Search title or horns no." onChange={(e)=>this.findHorned(e)} />
+                <>
+            <Row xs={1} sm={2} md={3} className="g-4">
+                {
+                    (((horned2.filter(beastInfo => (beastInfo.horns == this.state.value)) !='')? horned2.filter(beastInfo => beastInfo.horns == this.state.value):
+                    horned2.filter(beastInfo => (beastInfo.title.toLowerCase().includes(this.state.value.toLowerCase())) !=[])? horned2.filter(beastInfo =>beastInfo.title.toLowerCase().includes(this.state.value.toLowerCase())):
+                     horned2)).map( beast => 
+                        <Col key={beast._id}>
+                <Cards id={beast._id} title={beast.title} image_url={beast.image_url} description={beast.description} />
+            </Col>
+            )
+            }
+            </Row>
+            </>
+            </div>
         )
     }
 }
